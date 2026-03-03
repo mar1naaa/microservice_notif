0a8030c488b13a14db69d91eaf72b85664f17f26
mar1naaa <mazh607@gmail.com>
2026-03-03 17:15:12 +0300

    first

---
+++ products.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	Name     string
+	ImageURL string
+}
+
+type PageData struct {
+	Products []Product
+}
+
+func main() {
+	// 1) mux — маршрутизатор
+	mux := http.NewServeMux()
+
+	// 2) Раздача статических файлов:
+	// URL:  /static/...
+	// Disk: ./uploads/...
+	fs := http.FileServer(http.Dir("./uploads"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// 3) Главная страница (HTML)
+	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData{
+			Products: []Product{					
